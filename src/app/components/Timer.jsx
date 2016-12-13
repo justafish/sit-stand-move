87188e9b00ef728f30db4c1b997f5dbebd70f041
Sally Young <sally@justafish.co.uk>
2016-12-13 13:39:25 +0100

    Icons and quit functionality

--- src/app/components/Timer.jsx
+++ src/app/components/Timer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import format from 'date-fns/format';
+import path from 'path';
 
 class Timer extends React.Component {
   constructor() {
@@ -14,7 +15,9 @@ class Timer extends React.Component {
   tick() {
     if (this.timeToNextState() <= 0) {
       // Change state.
-      new Notification(this.state.states[this.nextState()]);
+      new Notification(this.getNextStateTitle(), {
+        icon: path.join('icons', this.state.states[this.nextState()] + '.png')
+      });
       this.setState({
         currentState: this.nextState(),
         start: Date.now(),
@@ -32,19 +35,38 @@ class Timer extends React.Component {
   }
   stopTimer() {
     clearInterval(this.timer);
-    this.setState({start: null, elapsed: null})
+    this.setState({currentState: 0, start: null, elapsed: null})
   }
   timeToNextState() {
-    return (this.props[this.state.states[this.state.currentState]] * 60000) - this.state.elapsed;
+    let time = (this.props[this.state.states[this.state.currentState]] * 60000) - this.state.elapsed;
+    return time >= 0 ? time : 0;
   }
   nextState() {
     return this.state.currentState + 1 >= this.state.states.length ? 0 : this.state.currentState + 1;
   }
+  getNextStateTitle() {
+    let state = this.state.states[this.nextState()];
+    state = state.charAt(0).toUpperCase() + state.slice(1);
+
+    return state;
+  }
   render() {
     return (
-      <div className="timer">
-        {this.state.start ?  <button onClick={() => this.stopTimer()}>Stop</button> : <button onClick={() => this.startTimer()}>Start</button>}
-        {this.state.states[this.nextState()]} in {format(this.timeToNextState(), 'm[m] s[s]')}
+      <div className="timer container-fluid">
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <div className="panel-title">
+                  {this.state.start ?  <button type="button" className="btn btn-danger btn-lg" onClick={() => this.stopTimer()}>Stop</button> : <button type="button" className="btn btn-success btn-lg" onClick={() => this.startTimer()}>Start</button>}
+                </div>
+              </div>
+              <div className="panel-body">
+                <h4>{this.getNextStateTitle()} in {format(this.timeToNextState(), 'm[m] s[s]')}</h4>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import menubar from 'menubar';
+const {app, Menu} = require('electron');
 
-var mb = menubar({
-  'node-integration': false
+let mb = menubar({
+  'node-integration': false,
+  width: 500,
+  height: 150
+});
+
+mb.on('ready', function ready () {
+  let contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Quit',
+      type: 'normal',
+      click: function() { app.quit(); }
+    }
+  ]);
+  mb.tray.on('right-click', function() {
+    mb.tray.popUpContextMenu(contextMenu);
+  });
 });
